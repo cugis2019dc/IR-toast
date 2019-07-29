4216dc6b34bddc7de6840301f335651049cdb2e4
IrisReyes21 <53440559+IrisReyes21@users.noreply.github.com>
2019-07-29 13:54:54 -0400

    Add files via upload

---
+++ introtopythonandspyder.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5*2
+5/2
+5-2
+5**2
+(8/9)*3
+5*2
+def multiply(a,b):
+    multiply = a*b
+ 
+    print(multiply)
+        
+multiply (4,5)
+    
+def sum (a,b,c):
+    sum = a+b+c
+    
+    print(sum)
+
+sum (2,4,6)
+
+def area (b,h): #area of triangle
+    area = (0.5)*b*h
+
+    print("The area of the triangle with the base", b ,"and the height", h ,"is",area)
+
+area (5,4)
